29938575640ff15e8a512f28f3c6a4d18724ca75
irvinafiradila <irvinafiradila74@gmail.com>
2016-11-13 18:31:34 +0700

    Third commit

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl505142332/itjob/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl505142332/itjob/MainActivity.java
@@ -2,12 +2,28 @@ package id.sch.smktelkom_mlg.project.xirpl505142332.itjob;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bOk = (Button) findViewById(R.id.buttonOk);
+
+        findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
     }
 }
